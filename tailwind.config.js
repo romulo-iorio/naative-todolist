82de389f3397357fef792346aae8682c20e215e3
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-18 15:47:54 -0300

    feat: configuring tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
