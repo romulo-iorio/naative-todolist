36a8d7af9a664ed3457ccf5ee0b26f736465db20
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-18 15:48:21 -0300

    feat: creating global-styles file to put the design rules

---
+++ src/global-styles.ts
@@ -0,0 +1,29 @@
+export const globalStyles = {
+  fontFamily: {
+    heading: "Inter_600SemiBold",
+    subtitle: "Inter_500Medium",
+    body: "Inter_400Regular",
+    bold: "Inter_700Bold",
+  },
+  colors: {
+    "blue-dark": "#1e6f9f",
+    blue: "#4ea8de",
+    "purple-dark": "#5e60ce",
+    purple: "#8284fa",
+    gray: {
+      100: "#f2f2f2",
+      200: "#d9d9d9",
+      300: "#808080",
+      400: "#333",
+      500: "#262626",
+      600: "#1a1a1a",
+      700: "#0d0d0d",
+    },
+    danger: "#e25858",
+  },
+  fontSize: {
+    sm: 12,
+    md: 14,
+    lg: 16,
+  },
+};
