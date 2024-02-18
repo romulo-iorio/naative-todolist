0e3b375a0025cde7287d13e44ef14fd7c2e7e3dc
Romulo Iorio <romuloiorio@hotmail.com>
2024-02-17 23:36:15 -0300

    feat: creating Loding component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import { ActivityIndicator, View } from "react-native";
+import colors from "tailwindcss/colors";
+
+export const Loading = () => (
+  <View className="flex-1 items-center justify-center bg-slate-900">
+    <ActivityIndicator color={colors.white} />
+  </View>
+);
